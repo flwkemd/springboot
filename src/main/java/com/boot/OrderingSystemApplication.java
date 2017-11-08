1d6f3a8323a5a6471731fbf96966190bcf1da5b4
flwkemd <flwkemd@naver.com>
2017-11-08 02:16:23 +0100

    initial commit

---
+++ src/main/java/com/boot/OrderingSystemApplication.java
@@ -0,0 +1,28 @@
+package com.boot;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class OrderingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderingSystemApplication.class, args);
+	}
+	
+	/** 
+	 * SqlSessionFactory ¼³Á¤
+	 */
+	
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource)throws Exception{
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+            sessionFactory.setDataSource(dataSource);
+            return sessionFactory.getObject();
+    }
+}
